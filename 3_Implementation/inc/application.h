9b186e119e1d0db9dd951c208e7cd3763154ea9a
ACE <94338447+SachinSingh07@users.noreply.github.com>
2021-11-21 17:03:41 +0530

    Add files via upload

---
+++ 3_Implementation/inc/application.h
@@ -0,0 +1,12 @@
+#include"header.h"
+  
+/* Function Prototype */
+void menu();
+void got();
+void start();
+void back();
+void addrecord();
+void listrecord();
+void modifyrecord();
+void deleterecord();
+void searchrecord();
