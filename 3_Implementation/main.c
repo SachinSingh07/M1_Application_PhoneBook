2ee3d1936864cb6977870b2cdec996d2265fdeb1
ACE <94338447+SachinSingh07@users.noreply.github.com>
2021-11-21 17:04:26 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+#include"conio.h"
+#include"string.h"
+#include"stdlib.h"
+#include"windows.h"
+#include"header.h"
+#include"application.h"
+
+int main()
+{
+    system("color 5f");
+    start();
+    return 0;
+}
