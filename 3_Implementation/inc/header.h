87bf499eb198797a7f809c968612dfef58ebbb84
ACE <94338447+SachinSingh07@users.noreply.github.com>
2021-11-21 17:01:39 +0530

    Add files via upload

---
+++ 3_Implementation/inc/header.h
@@ -0,0 +1,28 @@
+/**
+ * @file header.h
+ * @author Sachin Singh 
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef HEADER_H
+#define HEADER_H
+
+
+struct person
+{
+    char name[35];
+    char address[50];
+     char father_name[35];
+     char mother_name[30];
+    long int mble_no;
+    char sex[8];
+    char mail[100];
+    char citision_no[20];
+
+    };
+  
+#endif //#ifndef HEADER_H
