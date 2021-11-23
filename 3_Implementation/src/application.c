fe7b6e8a65cc5c2e274ce6ac3a42e6418ba2c800
ACE <94338447+SachinSingh07@users.noreply.github.com>
2021-11-23 19:51:24 +0530

    Add files via upload

---
+++ 3_Implementation/src/application.c
@@ -0,0 +1,184 @@
+#include"stdio.h"
+#include"stdlib.h"
+#include"string.h"
+#include"application.h"
+
+
+
+void add()
+{
+	FILE *fp;
+	fp = fopen("mohsin.txt","a");	
+		
+	system("clear");
+	printf("\n\n\n\n\t\t\tENTER THE CONTACT\n\n");
+
+	printf("\t\tenter the name : ");
+	scanf("%s",s.name);
+	printf("\n\t\tenter mobile no. :");
+	scanf("%llu",&s.mobile_no);
+	printf("\n\t\tenter sex : ");
+	scanf("%s",s.sex);
+	printf("\n\t\tEnter email id : ");
+	scanf("%s",s.email);
+	printf("\n\t\tenter Address : ");
+	scanf("%s",s.address);
+
+	fwrite(&s, sizeof(s), 1, fp);
+	
+	
+	fclose(fp);
+}
+
+
+
+void display()
+{
+	FILE *fp1;
+	fp1 = fopen("mohsin.txt","r");
+	system("clear");
+	if(fp1 == NULL)
+		printf("\t\tNO CONTACT\n");
+	else
+	{	
+		while(fread(&s, sizeof(s), 1, fp1))
+			printf("\n\t\tNAME = %s\n\t\tMOBILE NO. = %llu\n\t\tSEX = %s\n\t\tEMAIL ID = %s\n\t\tADDRESS = %s\n\n",s.name,s.mobile_no,s.sex,s.email,s.address);
+	}
+	
+	fclose(fp1);
+}
+
+
+
+
+void search()
+{
+	char search[20];
+	int k=1,t=0;
+	system("clear");
+	FILE *fp2;
+
+	printf("\n\n\n\n\n\n\t\t\t\t\tENTER THE NAME WHICH YOU WANT TO SERCH\n\n\n\t\t\t\t\t\t  NAME :-");
+	scanf("%s",search);
+	
+	fp2 = fopen("mohsin.txt","r");
+	while(fread(&s, sizeof(s), 1, fp2))
+	{
+		k = strcmp(search,s.name);
+		if(k == 0)
+		{
+			t = 1;
+			printf("\n\t\tNAME = %s\n\t\tMOBILE NO. = %llu\n\t\tSEX = %s\n\t\tEMAIL ID = %s\n\t\tADDRESS = %s\n\n",s.name,s.mobile_no,s.sex,s.email,s.address);
+		
+		}
+	}
+	if(t == 0)
+		printf("\n\n\n\n\n\n\n\n\t\t\t\t\t        CONTACT NOT FOUND\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+
+	fclose(fp2);		
+}
+
+void update()
+{
+	system("clear");
+	struct contact s1;
+	char search[20];
+	int k=1,t=0;
+	FILE *fp5,*fp6;
+
+	printf("\n\n\n\n\n\n\t\t\t\t\tENTER THE NAME WHICH YOU WANT TO EDIT\n\n\n\t\t\t\t\t\t  NAME :-");
+	scanf("%s",search);
+	
+	fp5 = fopen("mohsin.txt","r");
+	fp6 = fopen("temp.txt","w");
+
+	while(fread(&s, sizeof(s), 1, fp5))
+	{
+		k = strcmp(search,s.name);
+		if(k == 0)
+		{
+			t=1;
+			printf("\n\t\t\t\t\tPREVIOUS DATA\n");
+			printf("\n\t\tNAME = %s\n\t\tMOBILE NO. = %llu\n\t\tSEX = %s\n\t\tEMAIL ID = %s\n\t\tADDRESS = %s\n\n",s.name,s.mobile_no,s.sex,s.email,s.address);
+			printf("\n\n\n\n\t\t\t\t\tEDIT YOUR DATA\n\n");
+			
+			printf("\t\tenter the name : ");
+			scanf("%s",s1.name);
+			printf("\n\t\tenter mobile no. :");
+			scanf("%llu",&s1.mobile_no);
+			printf("\n\t\tenter sex : ");
+			scanf("%s",s1.sex);
+			printf("\n\t\tEnter email id : ");
+			scanf("%s",s1.email);
+			printf("\n\t\tenter Address : ");
+			scanf("%s",s1.address);
+
+			fwrite(&s1, sizeof(s1), 1, fp6);
+			continue;	
+		}
+		fwrite(&s, sizeof(s), 1, fp6);
+	}
+
+	if(t == 0)
+		printf("\n\n\n\n\n\n\n\n\t\t\t\t\t        CONTACT NOT FOUND\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+	if(t == 1)
+	{
+	remove("mohsin.txt");
+	fclose(fp5);
+	fclose(fp6);
+
+	fp5 = fopen("mohsin.txt","w");
+	fp6 = fopen("temp.txt","r");
+
+	while(fread(&s, sizeof(s), 1, fp6))
+		fwrite(&s,sizeof(s),1,fp5);
+
+			printf("\n\n\n\n\n\n\n\n\t\t\t\t\t        EDIT SUCCESSFULLY\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+	}
+	fclose(fp5);
+	fclose(fp6);
+		
+}
+
+void delete()
+{
+	system("clear");
+	FILE *fp3,*fp4;
+	char search[20];
+	int k=1,t=0;
+
+	fp3 = fopen("mohsin.txt","r");
+	fp4 = fopen("temp.txt","w");
+
+	printf("\n\n\n\n\n\n\t\t\t\t\tENTER THE NAME WHICH YOU WANT TO DELETE\n\n\n\t\t\t\t\t\t  NAME :-");
+	scanf("%s",search);
+	
+	while(fread(&s, sizeof(s), 1, fp3))
+	{
+		k = strcmp(search,s.name);
+		if(k == 0)
+		{
+			t=1;
+			continue;
+		}
+		fwrite(&s, sizeof(s), 1, fp4);
+	}
+	if(t == 0)
+		printf("\n\n\n\n\n\n\n\n\t\t\t\t\t        CONTACT NOT FOUND\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+	if(t == 1)
+	{
+	remove("mohsin.txt");
+	fclose(fp3);
+	fclose(fp4);
+
+	fp3 = fopen("mohsin.txt","w");
+	fp4 = fopen("temp.txt","r");
+
+	while(fread(&s, sizeof(s), 1, fp4))
+		fwrite(&s,sizeof(s),1,fp3);
+
+		printf("\n\n\n\n\n\n\n\n\t\t\t\t\t        DELETE SUCCESSFULLY\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+	}
+	fclose(fp3);
+	fclose(fp4);
+}
