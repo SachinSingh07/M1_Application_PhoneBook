ca82a7c715378480b6d8480ad887b7de4980f268
ACE <94338447+SachinSingh07@users.noreply.github.com>
2021-11-23 12:20:28 +0530

    Update header.h

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -24,5 +24,17 @@ struct person
     char citision_no[20];
 
     };
+ 
+  
+/* Function Prototype */
+void menu();
+void got();
+void start();
+void back();
+void addrecord();
+void listrecord();
+void modifyrecord();
+void deleterecord();
+void searchrecord();
   
 #endif //#ifndef HEADER_H
